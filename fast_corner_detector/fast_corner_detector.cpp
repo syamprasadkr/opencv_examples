6b0ae236d4d975944eec59a1fa10b0edf3e8b43f
syamprasadkr <syamkrajagopalan@gmail.com>
2019-01-14 10:41:00 -0500

    code commented

--- fast_corner_detector/fast_corner_detector.cpp
+++ fast_corner_detector/fast_corner_detector.cpp
@@ -6,25 +6,32 @@
 
 int main()
 {
+	// Video input
 	cv::VideoCapture cap(0);
 	if(!cap.isOpened())
 	{	
 		std::cout << "Invlaid Video Source\n";
 	}
+	
+	// Variables for video capture/ display and FAST algorithm
 	cv::Mat frame;
 	std::vector<cv::KeyPoint> corners;
-	int thresh = 10;
+	int thresh = 35;
 	bool non_max_suppression = true;
 	int type = cv::FastFeatureDetector::TYPE_9_16;
+	// Create FAST Feature Detector
 	auto fast_detector = cv::FastFeatureDetector::create(thresh, non_max_suppression, type);
 	std::string win0 = "FAST Corner Detector";
 	cv::namedWindow(win0, cv::WindowFlags::WINDOW_AUTOSIZE);
 	while(1)
 	{
 		cap >> frame;
+		// Perform detection of corner or keypoints
 		fast_detector -> detect(frame, corners);
+		// Draw the keypoints onto the frame and display
 		cv::drawKeypoints(frame, corners, frame, cv::Scalar(0, 0, 255));
 		cv::imshow(win0, frame);
+		// End the video and code when 'ESC' is pressed
 		if(cv::waitKey(10) == 27)
 		{
 			break;
